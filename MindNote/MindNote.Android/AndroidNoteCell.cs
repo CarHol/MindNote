198e74502088b796d1bc1aba98894b0654aaa88d
carhol <carhol@student.chalmers.se>
2020-07-23 15:39:22 +0200

    Structural fixes

---
+++ MindNote/MindNote.Android/AndroidNoteCell.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.Support.V4.Text;
+using Android.Text;
+using Android.Text.Method;
+using Android.Text.Style;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using MindNote;
+using MindNote.Droid;
+
+namespace MindNote.Droid
+{
+    internal class AndroidNoteCell : LinearLayout, INativeElementView
+    {
+        // Fields
+        public NoteCell NoteCell { get; private set; }
+        public Element Element => NoteCell;
+        Context context;
+
+        // Elements
+        public TextView ContentText { get; set; }
+        // Constructor
+        public AndroidNoteCell(Context context, NoteCell cell) : base(context)
+        {
+            NoteCell = cell;
+            this.context = context;
+
+            var view = (context as Activity).LayoutInflater.Inflate(Resource.Layout.AndroidNoteCell, null);
+            ContentText = view.FindViewById<TextView>(Resource.Id.ContentText);
+            
+            AddView(view);
+        }
+
+        public void UpdateCell(NoteCell cell)
+        {
+            SpannableStringBuilder ssb = new SpannableStringBuilder();
+            
+
+            //ContentText.Text = cell.Text;
+            //ssb.Append(cell.Text);
+            //ssb.SetSpan(new ImageSpan(context, Resource.Drawable.xamarin_logo), ssb.Length() - 1, ssb.Length(), 0);
+            //ssb.Append(" POST TEXT");
+            // Do layout stuff
+            //string testString = "<h1>TestHeading</h1><p>This is some text with <a href=\"https://www.google.com\">link</a> included.</p>";
+            ContentText.TextFormatted = HtmlCompat.FromHtml(cell.Text, HtmlCompat.FromHtmlModeLegacy);
+            ContentText.MovementMethod = LinkMovementMethod.Instance;
+            //ContentText.SetMovementMethod(LinkMovementMethod.getInstance());
+            //ContentText.TextFormatted = ssb;
+        }
+    }
+}
---
+++ MindNote/MindNote.Android/AndroidNoteRenderer.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+using System.ComponentModel;
+using Android.Content;
+using Android.Views;
+using Android.Widget;
+using MindNote;
+using MindNote.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(NoteCell), typeof(AndroidNoteRenderer))]
+namespace MindNote.Droid
+{
+    class AndroidNoteRenderer : ViewCellRenderer
+    {
+        AndroidNoteCell cell;
+
+        protected override Android.Views.View GetCellCore(Cell item, Android.Views.View convertView, ViewGroup parent, Context context)
+        {
+            var noteCell = (NoteCell)item;
+            Console.WriteLine("\t\t" + noteCell.Text);
+
+            cell = convertView as AndroidNoteCell;
+            if (cell == null)
+            {
+                cell = new AndroidNoteCell(context, noteCell);
+            }
+            else
+            {
+                cell.NoteCell.PropertyChanged += OnNoteCellProprtyChanged;
+            }
+
+            noteCell.PropertyChanged += OnNoteCellProprtyChanged;
+            cell.LongClickable = true;
+            cell.LongClick += (s, ea) =>
+            {
+                if (noteCell != null)
+                {
+                    noteCell.LongPressedHandler?.Invoke(noteCell, ea);
+                    var command = noteCell.LongpressCommand;// CustomImage.GetCommand(_view);  
+                    command?.Execute(noteCell);
+
+                }
+            };
+            cell.UpdateCell(noteCell);
+            return cell;
+        }
+
+        void OnNoteCellProprtyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var noteCell = (NoteCell)sender;
+            if (e.PropertyName == NoteCell.TextProperty.PropertyName)
+            {
+                cell.ContentText.Text = noteCell.Text;
+            }
+
+        }
+
+
+    }
+}
